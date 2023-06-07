aca04da88521050994d82dd0b24dc0afeae46fcc
kozhushna <k.kozhushna@gmail.com>
2023-06-07 18:38:59 +0300

    Add logout styles

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import RegistrationScreen from './src/Screens/RegistrationScreen';
 import LoginScreen from './src/Screens/LoginScreen';
 import CreatePostScreen from './src/Screens/CreatePostsScreen';
+import LogOut from './src/Icons/LogOut.svg';
 
 const MainStack = createStackNavigator();
 
@@ -37,6 +39,32 @@ export default function App() {
             title: 'Публікації',
             headerShown: true,
             headerTitleAlign: 'center',
+            headerStyle: {
+              borderBottomWidth: 1,
+              borderBottomColor: 'rgba(0, 0, 0, 0.3)',
+            },
+            headerTintColor: '#212121',
+            headerTitleStyle: {
+              fontFamily: 'Roboto',
+              fontWeight: 500,
+              fontSize: 17,
+              lineHeight: 22,
+            },
+            headerRight: () => {
+              const navigation = useNavigation();
+              return (
+                <TouchableOpacity
+                  style={styles.button}
+                  title="Log Out"
+                  onPress={() => navigation.navigate('Login')}
+                  color="white"
+                >
+                  <View style={styles.buttonContainer}>
+                    <LogOut width={24} height={24} />
+                  </View>
+                </TouchableOpacity>
+              );
+            },
           }}
         />
       </MainStack.Navigator>
@@ -45,9 +73,14 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  button: {
+    width: 25,
+    height: 25,
+    marginRight: 10,
+  },
+
+  buttonContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/Screens/CreatePostsScreen.js
+++ src/Screens/CreatePostsScreen.js
@@ -2,21 +2,39 @@ import React from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
 import UserImage from '../Images/User.png';
 
-const CreatePostScreen = () => (
-  <View style={styles.container}>
-    <Image source={UserImage} />
-    <Text style={styles.title}>Natali Romanova</Text>
-    <Text style={styles.text}>email@example.com</Text>
-  </View>
-);
+const CreatePostScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.holder}>
+        <Image source={UserImage} />
+        <View style={styles.textHolder}>
+          <Text style={styles.title}>Natali Romanova</Text>
+          <Text style={styles.text}>email@example.com</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
+    height: '100%',
     paddingBottom: 32,
     paddingTop: 32,
     paddingHorizontal: 16,
     backgroundColor: '#FFFFFF',
   },
+
+  holder: {
+    flex: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  textHolder: {
+    flex: 1,
+  },
+
   title: {
     fontFamily: 'Roboto',
     fontWeight: 700,
